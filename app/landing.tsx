60692c3ed6237472644170d7d1b48d6f281a8f3d
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2025-08-10 11:04:41 +0200

    Ajout de l'écran de landing et mise à jour de la navigation dans le layout

---
+++ app/landing.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function LandingScreen() {
+	return (
+		<View>
+			<Text>Landing Screen</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({});
